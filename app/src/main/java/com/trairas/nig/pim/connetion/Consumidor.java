f4d99285094e6db8beeb002f9038730e847d9474
Marcos001 <santosMsantos01@gmail.com>
2017-06-17 10:04:49 -0300

    com pacote connection

---
+++ app/src/main/java/com/trairas/nig/pim/connetion/Consumidor.java
@@ -0,0 +1,129 @@
+package com.trairas.nig.pim.connetion;
+
+/**
+ * Created by mrv on 24/04/17.
+ */
+
+import com.rabbitmq.client.*;
+import java.io.IOException;
+
+public class Consumidor {
+
+    private final static String QUEUE_NAME = "hello";
+
+    public Consumidor(){
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+
+                try{
+                    ConnectionFactory factory = new ConnectionFactory();
+                    factory.setHost("localhost");
+                    Connection connection = factory.newConnection();
+                    Channel channel = connection.createChannel();
+
+                    channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+                    System.out.println(" [*] Aguardando mensagens. To exit press CTRL+C");
+
+                    Consumer consumer = new DefaultConsumer(channel) {
+                        @Override
+                        public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)
+                                throws IOException {
+                            String message = new String(body, "UTF-8");
+
+                            String ip = "";
+                            String msg = "";
+                            String tmp = "";
+
+                            for(int i = 0; i < message.length(); i++){
+
+                                if(message.charAt(i)==':'){
+                                    ip = tmp;
+                                    tmp = "";
+                                }
+
+                                else if(message.charAt(i)=='§'){
+                                    msg = tmp;
+                                    tmp = "";
+                                }
+
+                                else if(message.charAt(i)!=':'){
+                                    tmp += message.charAt(i);
+                                }
+                            }
+
+                            System.out.println("IP = "+ip+"\n"+"Nome = "+msg);
+                            System.out.println(" [x] Received '" + message + "'");
+
+                            System.out.println("Obtidos os dados");
+                            String finalMsg = msg;
+                            String finalIp = ip;
+
+                            new Thread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    new Produtor(finalIp, finalMsg);
+                                    new Produtor(finalMsg);
+                                }
+                            }).start();
+                            System.out.println("mensagem Enviada "+msg+" a "+ip);
+
+                        }
+                    };
+                    channel.basicConsume(QUEUE_NAME, true, consumer);
+                }catch (Exception erro){
+                    System.out.println("NAO FOI");
+                }
+
+            }
+        }).start();
+
+    }
+
+    public Consumidor(byte[] fileData){
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+
+                try{
+                    ConnectionFactory factory = new ConnectionFactory();
+                    factory.setHost("localhost");
+                    Connection connection = factory.newConnection();
+                    Channel channel = connection.createChannel();
+
+                    channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+                    System.out.println(" [*] Aguardando mensagens. To exit press CTRL+C");
+
+                    Consumer consumer = new DefaultConsumer(channel) {
+                        @Override
+                        public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)
+                                throws IOException {
+
+                            String message = new String(body, "UTF-8");
+
+
+                            System.out.println(" [x] Received '" + message + "'");
+
+                            System.out.println("Obtidos os dados");
+
+
+                        }
+                    };
+
+                }catch (Exception erro){
+                    System.out.println("NAO FOI");
+                }
+
+            }
+        }).start();
+
+    }
+
+
+
+
+}
---
+++ app/src/main/java/com/trairas/nig/pim/connetion/Produtor.java
@@ -0,0 +1,124 @@
+package com.trairas.nig.pim.connetion;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+
+/**
+ * Created by mrv on 24/04/17.
+ */
+public class Produtor {
+
+    private final static String QUEUE_NAME = "enviar";
+
+    static final int PORT = Integer.getInteger("amqp.port", 5672);
+    static final String EXCHANGE = System.getProperty("amqp.exchange", "systemExchange");
+    static final String ENCODING = "UTF-8";
+
+    Connection connection;
+    //String QUEUE_NAME = "hello";
+
+
+    public Produtor(String message){
+
+
+        String HOST = System.getProperty("amqp.host", "192.168.0.106");
+
+        try{
+
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.setHost(HOST);
+            factory.setPort(PORT);
+            factory.setUsername("nig");
+            factory.setPassword("nig");
+            factory.setVirtualHost("/");
+            connection = factory.newConnection();
+            Channel channel = connection.createChannel();
+
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));
+            System.out.println(" [x] Sent '" + message + "'");
+
+            channel.close();
+            connection.close();
+
+
+        }catch (Exception erro){
+            System.out.println("Erro ao instanciar publisher enviando ao cliente >"+HOST+" \n"+erro);
+        }
+
+
+    }
+
+    public Produtor(byte[] fileData){
+
+
+
+        String HOST = System.getProperty("amqp.host", "192.168.0.106");
+
+        try{
+
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.setHost(HOST);
+            factory.setPort(PORT);
+            factory.setUsername("nig");
+            factory.setPassword("nig");
+            factory.setVirtualHost("/");
+            connection = factory.newConnection();
+            Channel channel = connection.createChannel();
+
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            channel.basicPublish("", QUEUE_NAME, null, fileData);
+            System.out.println(" [x] Sent file ");
+
+            channel.close();
+            connection.close();
+
+
+        }catch (Exception erro){
+            System.out.println("Erro ao instanciar publisher enviando ao cliente >"+HOST+" \n"+erro);
+        }
+
+
+    }
+
+
+
+    public Produtor(String ip, String fuxico){
+
+        Connection connection;
+
+        System.out.println("Querendo Publicar com IP = "+ip);
+
+       String HOST = System.getProperty("amqp.host", ip);
+
+        try{
+
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.setHost(HOST);
+            factory.setPort(PORT);
+            factory.setUsername("nig");
+            factory.setPassword("nig");
+            factory.setVirtualHost("/");
+            connection = factory.newConnection();
+            Channel channel = connection.createChannel();
+
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            channel.basicPublish("", QUEUE_NAME, null, fuxico.getBytes("UTF-8"));
+            System.out.println(" [x] Sent '" + fuxico + "'");
+
+            channel.close();
+            connection.close();
+
+
+        }catch (Exception erro){
+            System.out.println("Erro ao instanciar publisher enviando ao cliente >"+HOST+" \n"+erro);
+        }
+
+
+    }
+
+
+
+}
